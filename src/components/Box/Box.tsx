9e3908df9818cc145c88dbbdcce695b072e5be9a
Saleh <shojaeisaleh@gmail.com>
2023-02-19 07:15:49 +0330

    :sparkles: chore: add status to box

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -3,7 +3,11 @@ import TBoxProps from "./Box.type";
 import styles from "./BoxStyles.module.scss";
 
 const Box = ({ status = "empty", children }: TBoxProps) => {
-  return <div className={styles["box"]}>{children}</div>;
+  return (
+    <div className={styles["box"]} aria-details={status}>
+      {children}
+    </div>
+  );
 };
 
 export default Box;
